49847358f39e6d5fd43111d9f88230fe15ff3b80
praditia12 <aditpraditia714@gamil.com>
2023-09-11 10:08:09 +0700

    React -useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
-import Todo from './components/Todo';
+import Card from './components/Card';
+import Input from './components/Input';
+import Button from './components/Button';
+import { useRef } from 'react';
 
 function App() {
+    const inputRef = useRef();
+
+    function handleClick() {
+        inputRef.current.focus();
+
+        // console.log(inputRef.current);
+    }
     return (
         <PlaceContentCenter>
-            <Todo />
+            <Card>
+                <Card.Title>UseRef</Card.Title>
+                <Card.Body>
+                    <Input ref={inputRef} />
+                    <Button onClick={handleClick}>Test</Button>
+                </Card.Body>
+            </Card>
         </PlaceContentCenter>
     );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
-export default function Input({ type = 'text', ...props }) {
+import { forwardRef } from 'react';
+
+const Input = forwardRef(({ type = 'text', ...props }, ref) => {
     return (
         <input
+            ref={ref}
             {...props}
             className={
                 'transition duration-300 w-full focus-outline-none focus:ring focus:ring-blue-200 focus:border-blue-200 border-slate-300 shadow-sm rounded-lg'
@@ -9,4 +12,8 @@ export default function Input({ type = 'text', ...props }) {
             type={type}
         />
     );
-}
+});
+
+Input.displayName = 'Input';
+
+export default Input;
